7c9ba4731b9f933b207c63f7571494ad9d27e768
Thomas Erik Gaspar <thomaserikgaspar@hotmail.com>
2025-01-10 13:45:12 -0300

    feat: Adicionado rota de marcas de motores e configurado interface de validação do json.

---
+++ src/server/controllers/marca_motor/TypesMarca.ts
@@ -0,0 +1,5 @@
+interface IMarca {
+    nome: string
+}
+
+export {IMarca};
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 
-import { MotoresController } from "./../controllers";
+import { MarcasController, MotoresController } from "./../controllers";
 
 const router = Router();
 
+//Rota de teste sem uso por hora
 router.get("/", (req, res) => {
     res.send("Olá mundo");
   });
 
 router.post("/motores", MotoresController.create);
+
+router.post("/marcas", MarcasController.create);
 
 
 export { router };
