7c947231f4a3f73946cd85f82e8bf2b26639ae95
Thomas Erik Gaspar <thomaserikgaspar@hotmail.com>
2025-02-03 16:32:23 -0300

    feat: Adicionado delete em todas as rotas.

--- src/server/controllers/carcaca/index.ts
+++ src/server/controllers/carcaca/index.ts
@@ -2,12 +2,12 @@ import * as create from "./Create";
 import * as getall from "./GetAll";
 import * as getbyid from "./GetById";
 import * as updatebyid from "./UpdateById";
-//import * as deletebyid from "./DeleteById";
+import * as deletebyid from "./DeleteById";
 
 export const CarcacaController = {
   ...create,
   ...getall,
   ...getbyid,
   ...updatebyid,
-  //...deletebyid,
+  ...deletebyid,
 };
